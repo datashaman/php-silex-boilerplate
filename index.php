8dbcde604b0ceaf57b45b31ea66094600c43b0e8
Marlin Forbes <marlinf@datashaman.com>
2013-04-29 15:25:35 +0200

    Add logging and favicon

--- index.php
+++ index.php
@@ -12,6 +12,10 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__.'/views',
 ));
 
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => __DIR__.'/logs/development.log'
+));
+
 $app->get('/', function() use($app) {
   return $app['twig']->render('hello.html', array(
     'name' => 'World'
